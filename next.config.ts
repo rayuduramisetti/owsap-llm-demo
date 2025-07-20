bc40f26f9ca01a6f47b8db1fe4c1d2a039a4f626
Rayudu Ramisetti <rayuduramisetti26@gmail.com>
2025-07-19 22:15:29 -0500

    config from amplify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    SIMULATE_RESPONSES: process.env.SIMULATE_RESPONSES,
+  },
 };
 
 export default nextConfig;
